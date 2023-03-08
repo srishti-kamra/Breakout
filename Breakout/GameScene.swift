3db892ba4ceb425f43734f48226d71a63e1f8332
Srishti Kamra <srishtikamra07@gmail.com>
2023-03-08 10:07:44 -0600

    Created Ball Object

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -12,12 +12,18 @@ class GameScene: SKScene {
     
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
-    
+    var ball = SKShapeNode()
     override func didMove(to view: SKView) {
      //this stuff happens once (when the app opens)
         createBackground()
+        resetGame()
         
         }
+    func resetGame(){
+        //this stuff happens before each game starts
+        makeBall()
+        
+    }
     
     func createBackground(){
         let stars = SKTexture(imageNamed: "Stars")
@@ -32,11 +38,40 @@ class GameScene: SKScene {
             let moveForever = SKAction.repeatForever(moveLoop)
             starsBackground.run(moveForever)
         }
+    }
+    
+    func makeBall() {
+        ball.removeFromParent() //remove the ball (if it exists)
+        ball = SKShapeNode(circleOfRadius: 10)
+        ball.position = CGPoint (x: frame.midX, y: frame.midY)
+        ball.strokeColor = .black
+        ball.fillColor = .yellow
+        ball.name = "ball"
+        
+        //physics shape matches ball images
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: 10)
+        //ignores all forces and impulses
+        ball.physicsBody?.isDynamic = false
+        //use precision collision detection
+        ball.physicsBody?.usesPreciseCollisionDetection = true
+        //no loss of energy from friction
+        ball.physicsBody?.friction = 0
+        //no g
+        ball.physicsBody?.affectedByGravity = false
+        //bounces fully off other objects
+        ball.physicsBody?.restitution = 1
+        //does not slow over time
+        ball.physicsBody?.linearDamping = 0
+        ball.physicsBody?.contactTestBitMask = (ball.physicsBody?.collisionBitMask)!
+        addChild(ball) //add ball object to the view 
+        
+        
         
         
         
         
     }
+    
     }
     
    
